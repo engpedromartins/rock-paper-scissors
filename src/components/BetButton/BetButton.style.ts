76cdd7646521beec3fea9a042d20615e12ab9d20
devMartinsPedro <pedro.martins@meutudo.app>
2023-06-04 21:54:20 -0300

    fix: change conditions betButton and modify style

--- src/components/BetButton/BetButton.style.ts
+++ src/components/BetButton/BetButton.style.ts
@@ -1,10 +1,40 @@
 import styled from 'styled-components';
+import { Choice } from '../../types/interface';
+import colors from '../../common/style';
 
-export const Button = styled.button<{ buttonColor: string }>`
-  background-color: ${(props) => props.buttonColor};
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
+interface ButtonProps {
+  title: Choice;
+}
+
+const Button = styled.button<ButtonProps>(({ title }) => {
+  let backgroundColor = '';
+  let color = '';
+
+  if (title === Choice.Rock) {
+    backgroundColor = colors.purple;
+    color = colors.blue;
+  } else if (title === Choice.Paper) {
+    backgroundColor = colors.darkGreen;
+    color = colors.green;
+  } else {
+    backgroundColor = colors.darkRed;
+    color = colors.red;
+  }
+
+  const buttonStyle = {
+    backgroundColor,
+    color,
+    border: `1px solid ${color}`,
+    borderRadius: '4px',
+    cursor: 'pointer',
+    padding: '20px',
+    transition: 'background-color 0.3s ease',
+    '&:hover': {
+      opacity: 0.9,
+    },
+  };
+
+  return buttonStyle;
+});
+
+export { Button };
--- src/components/BetButton/BetButton.tsx
+++ src/components/BetButton/BetButton.tsx
@@ -10,25 +10,14 @@ interface BetButtonProps {
 export default function BetButton(props: BetButtonProps) {
   const { onClick, title } = props
 
-  const getColorByTitle = (title: string): string => {
-    const colorMap: { [key: string]: string } = {
-      Rock: 'red',
-      Paper: 'blue',
-      Scissors: 'yellow',
-    };
-
-    return colorMap[title] || 'gray';
-  };
-
   const handleClick = () => {
     onClick(title as Choice);
   };
 
-  const buttonColor = getColorByTitle(title);
-
   return (
     <Button
-      buttonColor={buttonColor} onClick={handleClick}>
+      title={title}
+      onClick={handleClick}>
       {title}
     </Button >)
 }
