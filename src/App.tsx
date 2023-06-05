5c06ea6d23b94087c824191112154be95f5066f9
devMartinsPedro <pedro.martins@meutudo.app>
2023-06-04 22:40:07 -0300

    feat: create game compnent

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,16 @@
-import React, { useState } from 'react';
-import './App.css';
-import BetButton from './components/BetButton/BetButton';
+import React from 'react';
+import AppBar from './components/Header/Header';
+import GlobalStyle from "./styles/global";
+import Game from './components/Game/Game';
 
-enum Choice {
-  Rock = 'Rock',
-  Paper = 'Paper',
-  Scissors = 'Scissors',
-}
 
 const App: React.FC = () => {
-  const [balance, setBalance] = useState(5000);
-  const [playerChoice, setPlayerChoice] = useState<Choice | null>(null);
-  const [computerChoice, setComputerChoice] = useState<Choice | null>(null);
-  const [result, setResult] = useState('');
-  const [betAmount, setBetAmount] = useState(500);
-
-  const placeBet = (choice: Choice) => {
-    if (balance < betAmount) {
-      setResult('Insufficient balance for the bet.');
-      return;
-    }
-
-    setPlayerChoice(choice);
-    setComputerChoice(getRandomChoice());
-    determineResult(choice);
-  };
-
-  const getRandomChoice = (): Choice => {
-    const choices = [Choice.Rock, Choice.Paper, Choice.Scissors];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-  };
-
-  const determineResult = (player: Choice) => {
-    if (player === computerChoice) {
-      setResult('try again');
-      return;
-    }
-
-    const winConditions: [Choice, Choice][] = [
-      [Choice.Rock, Choice.Scissors],
-      [Choice.Paper, Choice.Rock],
-      [Choice.Scissors, Choice.Paper],
-    ];
-
-    const isWin = winConditions.some(([playerChoice, computerChoice]) =>
-      player === playerChoice && computerChoice === computerChoice
-    );
-
-    if (isWin) {
-      setResult('You won!');
-      setBalance((prevBalance) => prevBalance + (betAmount * 3));
-    } else {
-      setResult('You lost!');
-      setBalance((prevBalance) => prevBalance - betAmount);
-    }
-  };
-
   return (
-    <div className="App">
-      <h1>Rock, Paper, Scissors</h1>
-      <div className="balance">Balance: {balance}</div>
-      <div className="choices">
-        <BetButton
-          onClick={placeBet}
-          title={Choice.Rock} />
-        <BetButton
-          onClick={placeBet}
-          title={Choice.Paper} />
-        <BetButton
-          onClick={placeBet}
-          title={Choice.Scissors} />
-      </div>
-    </div>
+    <>
+      <GlobalStyle />
+      <AppBar />
+      <Game />
+    </>
   );
 };
 
---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { Choice } from "../../types/interface";
+import BetButton from "../BetButton/BetButton";
+
+export default function Game() {
+  const [balance, setBalance] = useState(5000);
+  const [playerChoice, setPlayerChoice] = useState<Choice | null>(null);
+  const [computerChoice, setComputerChoice] = useState<Choice | null>(null);
+  const [result, setResult] = useState('');
+  const [betAmount, setBetAmount] = useState(500);
+
+  const placeBet = (choice: Choice) => {
+    if (balance < betAmount) {
+      setResult('Insufficient balance for the bet.');
+      return;
+    }
+
+    setPlayerChoice(choice);
+    setComputerChoice(getRandomChoice());
+    determineResult(choice);
+  };
+
+  const getRandomChoice = (): Choice => {
+    const choices = [Choice.Rock, Choice.Paper, Choice.Scissors];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  };
+
+  const determineResult = (player: Choice) => {
+    if (player === computerChoice) {
+      setResult('It\'s a tie!');
+      return;
+    }
+
+    const winConditions: [Choice, Choice][] = [
+      [Choice.Rock, Choice.Scissors],
+      [Choice.Paper, Choice.Rock],
+      [Choice.Scissors, Choice.Paper],
+    ];
+
+    const isWin = winConditions.some(([playerChoice, computerChoice]) =>
+      player === playerChoice && computerChoice === computerChoice
+    );
+
+    if (isWin) {
+      setResult('You won!');
+      setBalance((prevBalance) => prevBalance + (betAmount * 3));
+    } else {
+      setResult('You lost!');
+      setBalance((prevBalance) => prevBalance - betAmount);
+    }
+  };
+  return (
+    <>
+      <div>
+        <BetButton
+          onClick={placeBet}
+          title={Choice.Rock} />
+        <BetButton
+          onClick={placeBet}
+          title={Choice.Paper} />
+        <BetButton
+          onClick={placeBet}
+          title={Choice.Scissors} />
+      </div>
+      {
+        playerChoice && computerChoice && (
+          <div className="result">
+            <p>Player choice: {playerChoice}</p>
+            <p>Computer choice: {computerChoice}</p>
+            <p>{result}</p>
+          </div>
+        )
+      }
+    </>
+  )
+}
